fb1075ad668f8f6be1ebf1f1db0db87e5f993a63
Petra Reznakova <68958723+petkaml91@users.noreply.github.com>
2021-03-20 16:28:47 +0000

    Úkol lekce 2

--- main.js
+++ main.js
@@ -1,1 +1,54 @@
 // tady je místo pro náš program
+
+let lev = document.querySelector("p");
+
+function priNajeti() {
+    lev.style.fontWeight = 'bold';
+}
+
+function priOdjeti() {
+    lev.style.fontWeight = 'normal';
+}
+
+function priKliknuti() {
+    lev.classList.toggle('cervena');
+}
+
+function zvetseni() {
+    let textKeZvetseni = lev.style.fontSize;
+    //console.log(textKeZvetseni);
+    textKeZvetseni = parseInt(textKeZvetseni);
+    textKeZvetseni++;
+    //console.log(textKeZvetseni);
+    lev.style.fontSize = textKeZvetseni.toString() + "px";
+}
+
+function prehraj(elementSelector) {
+    let audioElement = document.querySelector(elementSelector);
+    audioElement.play();
+}
+
+function zastav(elementSelector) {
+    let audioElement = document.querySelector(elementSelector);
+    audioElement.pause();
+}
+
+function ticho(elementSelector) {
+    let audioElement = document.querySelector(elementSelector);
+    audioElement.volume = 0;
+}
+
+function tissi(elementSelector) {
+    let audioElement = document.querySelector(elementSelector);
+    audioElement.volume = 0.5;
+}
+
+function normal(elementSelector) {
+    let audioElement = document.querySelector(elementSelector);
+    audioElement.volume = 1;
+}
+
+function zacatek(elementSelector) {
+    let audioElement = document.querySelector(elementSelector);
+    audioElement.currentTime = 0;
+}
